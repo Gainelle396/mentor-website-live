a0dc7e19f7018c9b1261f9bc9eed68b3558bee13
Tyler <tylerkaitombs@gmail.com>
2024-12-19 15:13:47 +0000

    Tried connecting database, didn't work. I give up.

--- portal/register.php
+++ portal/register.php
@@ -5,26 +5,26 @@ include 'connect.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //Sign Up
     if (isset($_POST['signup'])) {
-        $firstName = $_POST['fName'];
-        $lastName = $_POST['lName'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+        $firstName = $conn->real_escape_string($_POST['fName']);
+        $lastName = $conn->real_escape_string($_POST['lName']);
+        $email = $conn->real_escape_string($_POST['email']);
+        $password = $conn->real_escape_string($_POST['password']);
 
         //Hash password
-        $hashedPassword = md5($password);
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        // Check if email already exists
+        //Check if email already exists
         $checkEmailQuery = "SELECT * FROM users WHERE email='$email'";
         $result = $conn->query($checkEmailQuery);
 
         if ($result->num_rows > 0) {
             echo "Email address already taken";
         } else {
-            // Insert new user
+            //Insert new user
             $insertQuery = "INSERT INTO users (firstName, lastName, email, password)
                             VALUES ('$firstName', '$lastName', '$email', '$hashedPassword')";
             if ($conn->query($insertQuery) === TRUE) {
-                //Redirect to homepage
+                //Redirect
                 header("Location: index.html");
                 exit();
             } else {
@@ -35,25 +35,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     //Login
     if (isset($_POST['login'])) {
-        $email = $_POST['email'];
+        $email = $conn->real_escape_string($_POST['email']);
         $password = $_POST['password'];
 
-        //Hash password
-        $hashedPassword = md5($password);
-
-        //Check for matching email and password
-        $loginQuery = "SELECT * FROM users WHERE email='$email' AND password='$hashedPassword'";
+        //Check matching email
+        $loginQuery = "SELECT * FROM users WHERE email='$email'";
         $result = $conn->query($loginQuery);
 
         if ($result->num_rows > 0) {
+            //Verify password
             $row = $result->fetch_assoc();
-            $_SESSION['email'] = $row['email'];
-
-            //Redirect to homepage
-            header("Location: ../index.html");
-            exit();
+            if (password_verify($password, $row['password'])) {
+                //Redirect
+                $_SESSION['email'] = $row['email'];
+                header("Location: .//index.html");
+                exit();
+            } else {
+                echo "Incorrect password.";
+            }
         } else {
-            echo "Incorrect email or password.";
+            echo "Email not registered.";
         }
     }
 }
